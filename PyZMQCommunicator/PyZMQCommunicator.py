82d37c23cdbf88c60fb415c8c53f5f2f00d8d226
Jehyun Cha <dehan2@naver.com>
2018-04-27 16:21:29 +0900

    180427

--- PyZMQCommunicator/PyZMQCommunicator.py
+++ PyZMQCommunicator/PyZMQCommunicator.py
@@ -1,1 +1,22 @@
+import zmq
+import time
+from ZMQCommunicator import ZMQCommunicator
 
+communicator = ZMQCommunicator()
+communicator.bind_server("tcp://*:5556")
+communicator.connect_client("tcp://localhost:5555")
+
+for request in range(100):
+    message = communicator.receive()
+    print("Received msg: %s" % message)
+    communicator.reply_to_client("Well received: %s" % message)
+
+    print("Send message")
+    communicator.send("World")
+
+    time.sleep(1)
+    print("Waiting for receiving reply...")
+    message = communicator.receive_reply()
+    print("Received reply: %s" % message)
+    print("Cycle complete")
+    print("")
